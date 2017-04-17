9cfadd16faa426235ee424fe40d279f117b0113a
zhengwei.xu <zhengwei.xu@upchina.com>
2017-04-17 13:41:59 +0800

    添加代码语法检查

--- src/public/js/challenge.js
+++ src/public/js/challenge.js
@@ -8,7 +8,7 @@
         var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
             lint: { esversion: 6 },
             lineNumbers: true,
-            mode:challengeType === '0' ? 'text/html' : 'javascript',
+            mode: challengeType === '0' ? 'text/html' : 'javascript',
             theme: 'monokai',
             runnable: true,
             matchBrackets: true,
@@ -25,8 +25,9 @@
             var previewFrame = document.getElementById('preview');
             var preview = previewFrame.contentDocument || previewFrame.contentWindow.document;
             preview.open();
-            preview.write('<script src="//cdn.bootcss.com/chai/4.0.0-canary.1/chai.min.js"></script>');
             preview.write('<script src="//cdn.bootcss.com/jquery/3.1.1/jquery.min.js"></script>');
+            preview.write('<script src="//cdn.bootcss.com/chai/4.0.0-canary.1/chai.min.js"></script>');
+            preview.write('<script src="//cdn.bootcss.com/d3/4.7.4/d3.min.js"></script>');
             preview.write('<script src="/js/frame-runner.js"></script>');
             preview.write(editor.getValue());
             preview.close();
@@ -39,5 +40,14 @@
             frame.__runTests();
 
         });
+
+        var output = CodeMirror.fromTextArea(document.getElementById("output"), {
+            lineNumbers: false,
+            lineWrapping: true,
+            mode: 'javascript',
+            readOnly: 'nocursor',
+            theme: 'monokai'
+        })
+
     })
 })();
