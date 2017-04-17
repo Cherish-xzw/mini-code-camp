0d8a3d7dab9be6c8767f381cb4224b075e658924
zhengwei.xu <zhengwei.xu@upchina.com>
2017-04-17 17:38:56 +0800

    添加错误提示信息

--- src/server/controllers/frontend/course.js
+++ src/server/controllers/frontend/course.js
@@ -6,6 +6,8 @@ const _ = require('lodash');
 const Block = require('../../model/block');
 const Challenge = require('../../model/challenge');
 
+const util = require('../../utils');
+
 function CourseController(router) {
 
     /**
@@ -17,7 +19,10 @@ function CourseController(router) {
             .sort('superOrder')
             .sort('order')
             .then(blocks => {
-                res.render('course', {blocks: _.groupBy(blocks, 'superBlock')});
+                res.render('course', {
+                    title:"课程列表 - Code Camp",
+                    blocks: _.groupBy(blocks, 'superBlock')
+                });
             });
     });
 
@@ -26,7 +31,11 @@ function CourseController(router) {
      */
     router.get('/challenge/:id', function (req, res) {
         Challenge.findById(req.params.id).then(challenge => {
-            res.render('challenge', {challenge: challenge});
+            res.render('challenge', {
+                title:"课程详情 - Code Camp",
+                challenge: challenge,
+                tests:util.createTests(challenge.tests)
+            });
         });
     });
 
---
+++ src/server/utils/index.js
@@ -0,0 +1,22 @@
+
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+function createTests(tests) {
+    return tests
+        .map(test => {
+            if (typeof test === 'string') {
+                return {
+                    text: ('' + test).split('message: ').pop().replace(/\'\);/g, ''),
+                    testString: test
+                };
+            }
+            return test;
+        });
+}
+
+module.exports = {
+    createTests:createTests
+};
