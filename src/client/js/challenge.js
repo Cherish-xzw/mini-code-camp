9c2933f67ed3fada5c8c1533a96e3ff576f26ea5
Amor <xuzhengwei.amor@gmail.com>
2017-05-27 22:05:05 +0800

    添加运行结果的反馈

--- src/client/js/challenge.js
+++ src/client/js/challenge.js
@@ -1,52 +1,55 @@
 $(document).ready(() => {
-    let delay;
-    let challengeType = window.__state__.challengeType;
-    let tests = window.__state__.tests;
-    let editor = CodeMirror.fromTextArea(document.getElementById('code'), {
-        lint: {esversion: 6},
-        lineNumbers: true,
-        mode: challengeType === 0 ? 'text/html' : 'javascript',
-        theme: 'monokai',
-        runnable: true,
-        matchBrackets: true,
-        autoCloseBrackets: true,
-        lineWrapping: true,
-        gutters: ['CodeMirror-lint-markers']
-    });
-    editor.on('change', () => {
-        clearTimeout(delay);
-        delay = setTimeout(updatePreview, 300);
-    });
-    setTimeout(updatePreview, 300);
-    function updatePreview() {
-        let previewFrame = document.getElementById('preview');
-        let preview = previewFrame.contentDocument || previewFrame.contentWindow.document;
-        preview.open();
-        preview.write('<script src="//cdn.bootcss.com/jquery/3.1.1/jquery.min.js"></script>');
-        preview.write('<script src="//cdn.bootcss.com/chai/4.0.0-canary.1/chai.min.js"></script>');
-        preview.write('<script src="//cdn.bootcss.com/d3/4.7.4/d3.min.js"></script>');
-        preview.write('<script src="/js/frame-runner.js"></script>');
-        if (challengeType) {
-            // javascript
-            preview.write('<script>' + editor.getValue() + '</script>');
-        } else {
-            // html
-            preview.write(editor.getValue());
-        }
-        preview.close();
+  let delay;
+  let challengeType = window.__state__.challengeType;
+  let tests = window.__state__.tests;
+  let editor = CodeMirror.fromTextArea(document.getElementById('code'), {
+    lint: {
+      esversion: 6
+    },
+    lineNumbers: true,
+    mode: challengeType === 0 ? 'text/html' : 'javascript',
+    theme: 'monokai',
+    runnable: true,
+    matchBrackets: true,
+    autoCloseBrackets: true,
+    lineWrapping: true,
+    gutters: ['CodeMirror-lint-markers']
+  });
+  editor.on('change', () => {
+    clearTimeout(delay);
+    delay = setTimeout(updatePreview, 300);
+  });
+  setTimeout(updatePreview, 300);
+
+  function updatePreview() {
+    let previewFrame = document.getElementById('preview');
+    let preview = previewFrame.contentDocument || previewFrame.contentWindow.document;
+    const source = editor.getValue();
+    preview.__source = source;
+    preview.open();
+    preview.write('<script src="//cdn.bootcss.com/jquery/3.1.1/jquery.min.js"></script>');
+    preview.write('<script src="//cdn.bootcss.com/chai/4.0.0-canary.1/chai.min.js"></script>');
+    preview.write('<script src="//cdn.bootcss.com/d3/4.7.4/d3.min.js"></script>');
+    preview.write('<script src="/js/frame-runner.js"></script>');
+    if (challengeType) {
+      // javascript
+      preview.write('<script>' + source + '</script>');
+    } else {
+      // html
+      preview.write(source);
     }
+    preview.close();
+  }
 
-    $('#submitButton').click(function () {
-        let previewFrame = document.getElementById('preview');
-        let frame = previewFrame.contentDocument || previewFrame.contentWindow.document;
-        frame.__runTests(tests);
-    });
-
-    let output = CodeMirror.fromTextArea(document.getElementById('output'), {
-        lineNumbers: false,
-        lineWrapping: true,
-        mode: 'javascript',
-        readOnly: 'nocursor',
-        theme: 'monokai'
-    });
+  $('#submitButton').click(function () {
+    let previewFrame = document.getElementById('preview');
+    let frame = previewFrame.contentDocument || previewFrame.contentWindow.document;
+    try {
+      frame.__runTests(tests);
+      $('#result').modal();
+    } catch (e) {
+      console.log(e.message);
+      alert(e.message);
+    }
+  });
 });
--- src/client/js/frame-runner.js
+++ src/client/js/frame-runner.js
@@ -1,21 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
   let assert = chai.assert;
+  const source = document.__source;
 
   document.__runTests = function (tests) {
+    const code = source;
     tests = tests || [];
     tests.map(function (obj) {
       let test;
       let result;
-      try {
+      // try {
         test = eval(obj.testString);
         if (typeof test === 'function') {
 
         } else {
 
         }
-      } catch (e) {
-        console.error(e);
-      }
+      // } catch (e) {
+      //   console.error(e.message);
+      // }
     });
   };
 });
